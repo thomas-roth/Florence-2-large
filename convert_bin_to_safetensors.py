4d1d93407698bcdf4aa3730ecd88529ea8bc1819
thomas-roth <uruox@student.kit.edu>
2025-07-07 12:26:31 +0200

    adjust file paths for changes in last commit (move trained models back up to root dir)

--- convert_bin_to_safetensors.py
+++ convert_bin_to_safetensors.py
@@ -30,6 +30,6 @@ def convert_bin_to_safetensors(bin_path, safetensors_path):
     print(f"Converted {bin_path} to {safetensors_path}")
 
 if __name__ == "__main__":
-    bin_path = str(Path(__file__).absolute().parent) + "/pretrained/pytorch_model.bin"
-    safetensors_path = str(Path(__file__).absolute().parent) + "/pretrained/model.safetensors"
+    bin_path = str(Path(__file__).absolute().parent) + "/pytorch_model.bin"
+    safetensors_path = str(Path(__file__).absolute().parent) + "/model.safetensors"
     convert_bin_to_safetensors(bin_path, safetensors_path)
